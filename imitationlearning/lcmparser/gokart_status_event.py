bb77e6a179544eeb7a7a7639bd49f54e73ad6719
alezana <azanardi@ethz.ch>
2018-10-30 11:14:25 +0100

    blob parser parser

--- imitationlearning/lcmparser/gokart_status_event.py
+++ imitationlearning/lcmparser/gokart_status_event.py
@@ -3,10 +3,10 @@ from io import BytesIO
 
 
 class GokartStatusEvent:
-    length = 4
+    size = 4
 
     def __init__(self, bytes):
         buf = BytesIO(bytes)
         self._msg_fingerprint = buf.read(8)
-        assert struct.unpack(">i", buf.read(4))[0] == self.length
-        self.value = struct.unpack("<f", buf.read(self.length))[0]
+        assert struct.unpack(">i", buf.read(4))[0] == self.size
+        self.value = struct.unpack("<f", buf.read(self.size))[0]
--- imitationlearning/lcmparser/parser_main.py
+++ imitationlearning/lcmparser/parser_main.py
@@ -1,15 +1,17 @@
 import lcm
 
 from imitationlearning.lcmparser.gokart_status_event import GokartStatusEvent
+from imitationlearning.lcmparser.seye_aedvs_event import SeyeAeApsEvent
 
 if __name__ == '__main__':
     log_path = "/media/ale/dubi_usb1/20181005T135151_1cb189b4.lcm.00"
     log = lcm.EventLog(log_path, 'r')
     for event in log:
 
-        if "gokart.status.get" in event.channel:
+        if "gokart.status.get" in event.channel and False:
             # msg = BinaryBlob.decode(event.data)
             gokart_status_event = GokartStatusEvent(event.data)
 
-            # gokart_status_event = GokartStatusEvent(msg)
-            print("steering:", gokart_status_event.value)
+        if "seye.overview.aeaps" in event.channel:
+            print("reading events")
+            events_event = SeyeAeApsEvent(event.data)
