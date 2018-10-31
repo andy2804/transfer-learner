b690c70369a9bbec8f5804c2f5f65c61e4b52198
alezana <azanardi@ethz.ch>
2018-10-31 17:34:20 +0100

    test ci

--- semsegmentation/datasets/cityscapes_test.py
+++ semsegmentation/datasets/cityscapes_test.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 
-from semsegmentation.datasets.cityscapes import CITYSCAPES_LABELS, create_cityscapes_label_colormap
+from semsegmentation.datasets.cityscapes import Cityscapes
 
 
-class CitiscapesColormaps(tf.test.TestCase):
+class CityscapesTest(tf.test.TestCase):
+
+    def setUp(self):
+        self.dataset = Cityscapes()
 
     def testColormaps(self):
-        self.assertEqual(len(CITYSCAPES_LABELS), create_cityscapes_label_colormap().shape[0])
-        self.assertEqual(3, create_cityscapes_label_colormap().shape[1])
+        self.assertEqual(len(self.dataset.labels), self.dataset.colormap.shape[0])
+        self.assertEqual(3, self.dataset.colormap.shape[1])
 
 
 if __name__ == '__main__':
