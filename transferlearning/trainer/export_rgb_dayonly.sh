38f9e08d3424dc6c15af2b75d1e410d6d816beaa
andy2804 <andy2804@github.com>
2018-11-12 16:43:30 +0100

    Updated training configs

--- transferlearning/trainer/export_rgb_dayonly.sh
+++ transferlearning/trainer/export_rgb_dayonly.sh
@@ -5,11 +5,11 @@ export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 
 # Job_name can't have spaces
 job_name=dayonly
-ckpt_number=164085
+ckpt_number=0
 
 # Actual call
-CUDA_VISIBLE_DEVICES=0 python3 object_detection/export_inference_graph.py \
+CUDA_VISIBLE_DEVICES=0,1,2 python3 object_detection/export_inference_graph.py \
     --input_type image_tensor \
-    --pipeline_config_path $HOME/zauron/resources/nets_cfgs/configs/ssd_inceptionv2_$job_name.config \
-    --trained_checkpoint_prefix /shared_experiments/kaist/models/obj_detector_retrain/models/train_$job_name/model.ckpt-$ckpt_number \
-    --output_directory /shared_experiments/kaist/models/obj_detector_retrain/$job_name/
+    --pipeline_config_path $HOME/WormholeLearning/resources/nets_cfgs/configs/ssd_inceptionv2_$job_name.config \
+    --trained_checkpoint_prefix /media/sdc/andya/wormhole_learning/models/obj_detector_retrain/train_$job_name/model.ckpt-$ckpt_number \
+    --output_directory /media/sdc/andya/wormhole_learning/models/obj_detector_retrain/$job_name/
